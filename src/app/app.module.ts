6d3ee33ac03e960caa4d96310d563485f5da4a8b
GeorgeLBS1 <jj.diaz002@gmail.com>
2021-04-29 08:07:20 -0600

    Primera parte curso angular desde 0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesModule } from './Heroes/Heroes.module';
+import { contadorModule } from './contador/contador/contador.module';
+
+//Crear un módulo llamado contador module
+//Declaraciones y exportaciones
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    contadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
